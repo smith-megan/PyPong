1c8dbf0b784a8d4f099a44ff9885afde327f6961
Megan <smith.megan.design@gmail.com>
2021-10-19 17:17:29 -0600

    finished game

--- pong.py
+++ pong.py
@@ -1,5 +1,5 @@
 import turtle
-
+import winsound
 # initializing
 wn= turtle.Screen()
 wn.title("PONG")
@@ -32,6 +32,21 @@ ball.color("white")
 ball.penup()
 ball.goto(0,0)
 
+ball.dx=.2
+ball.dy=.2
+
+# score
+score_a=0
+score_b=0
+
+pen=turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A: 0 Plater B: 0", align="center", font=("Courier", 24, "normal"))
+
 # functions
 def paddle_1_up():
   y=paddle_1.ycor()
@@ -51,7 +66,66 @@ def paddle_1_down():
 wn.listen()
 wn.onkeypress(paddle_1_down, "s")
 
+
+def paddle_2_up():
+  y=paddle_2.ycor()
+  y += 20
+  paddle_2.sety(y)
+
+# keyboard binding
+wn.listen()
+wn.onkeypress(paddle_2_up, "Up")
+
+def paddle_2_down():
+  y=paddle_2.ycor()
+  y -= 20
+  paddle_2.sety(y)
+
+# keyboard binding
+wn.listen()
+wn.onkeypress(paddle_2_down, "Down")
+
 # main loop
 
 while True:
   wn.update()
+  #move ball
+  ball.setx(ball.xcor() + ball.dx)
+  ball.sety(ball.ycor()+ ball.dy)
+
+  #border check
+  if ball.ycor() > 290:
+    ball.sety(290)
+    ball.dy*= -1
+    winsound.PlaySound("noise.wav", winsound.SND_ASYNC)
+
+  if ball.ycor() < -290:
+    ball.sety(-290)
+    ball.dy*= -1
+    winsound.PlaySound("noise.wav", winsound.SND_ASYNC)
+    
+
+  if ball.xcor()>390:
+    ball.goto(0,0)
+    ball.dx*=-1
+    score_a +=1
+    pen.clear()
+    pen.write("Player A: {} Plater B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+  
+  if ball.xcor()< -390:
+    ball.goto(0, 0)
+    ball.dx*=-1
+    score_b +=1
+    pen.clear()
+    pen.write("Player A: {} Plater B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
+  # paddle ball collisions
+  if ball.xcor()>340 and ball.xcor()<350 and (ball.ycor()<paddle_2.ycor() + 40 and ball.ycor() > paddle_2.ycor() -40):
+    ball.setx(340)
+    ball.dx *=-1
+    winsound.PlaySound("noise.wav", winsound.SND_ASYNC)
+
+  if ball.xcor()<-340 and ball.xcor()<-350 and (ball.ycor()<paddle_1.ycor() + 40 and ball.ycor() > paddle_1.ycor() -40):
+    ball.setx(-340)
+    ball.dx *=-1
+    winsound.PlaySound("noise.wav", winsound.SND_ASYNC)
